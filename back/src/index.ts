e2fc65709db8970e239e9467c7cd0a3d7513bc67
luiggy <luiggylml@gmail.com>
2024-10-04 02:00:14 -0500

    chore: update config commitlint

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,7 +1,14 @@
 import express, { Request, Response } from 'express';
+import cors from 'cors';
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+const corsOptions = {
+    origin: '*'
+};
+
+app.use(cors(corsOptions));
 
 app.get('/', (req: Request, res: Response) => {
     res.json({ msg: 'Hello, this is your JSON response on Express' });
