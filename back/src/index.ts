2834f53a898e700be8cc2a2570025d4fce9f6e43
luiggy <luiggylml@gmail.com>
2024-10-04 01:39:34 -0500

    chore: add config vercel

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,1 +1,12 @@
-console.log("hola")
+import express, { Request, Response } from 'express';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req: Request, res: Response) => {
+    res.json({ msg: 'Hello, this is your JSON response on Express' });
+});
+
+app.listen(port, () => {
+    console.log(`App escuchando en http://localhost:${port}`);
+});
